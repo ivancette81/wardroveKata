8f0e524c500ac91ef2e4f8aed496a9a1d4060689
TOSH-0001\usuario local <imorales@list.es>
2021-04-29 13:02:17 +0200

    Test 50 and 75 sizes availables

--- wardroveKata/test.cpp
+++ wardroveKata/test.cpp
@@ -1,35 +1,57 @@
 #include "pch.h"
 #include <list>
+#include <algorithm>
 
 // wardroveKata
 // total length of 250cm
 // sizes: 50cm, 75cm, 100cm, and 120cm
-// test: combrinaciones si sólo dispongo del de 50cm
-// test: combrinaciones si dispongo de 50 y 75
-// test: combrinaciones si dispongo de 50, 75 y 100
-// test: combrinaciones si dispongo de 50, 75, 100 y 120
+// test: combinaciones si sólo dispongo del de 50cm
+// test: combinaciones si dispongo de 50 y 75
+// test: combinaciones si dispongo de 50, 75 y 100
+// test: combinaciones si dispongo de 50, 75, 100 y 120
 
 class Wall {
 private:
 	std::list<int> disponibleSizes;
-	std::list<int> combination;
+	std::list<std::list<int>> combinations;
 
+	const int WALL_SIZE = 250;
 public:
-	Wall(int size)
+	Wall(const std::list<int>& sizes)
 	{
-		disponibleSizes.push_back(size);
+		disponibleSizes = sizes;
+		disponibleSizes.sort();
 	}
 	void combine()
 	{
-		combination.push_back(50);
-		combination.push_back(50);
-		combination.push_back(50);
-		combination.push_back(50);
-		combination.push_back(50);
+		std::list<int> combination;
+		if (std::find(disponibleSizes.begin(), disponibleSizes.end(), 50) != disponibleSizes.end())
+		{
+			combination.push_back(50);
+			combination.push_back(50);
+			combination.push_back(50);
+			combination.push_back(50);
+			combination.push_back(50);
+			combinations.push_back(combination);
+		}
+		if (std::find(disponibleSizes.begin(), disponibleSizes.end(), 75) != disponibleSizes.end())
+		{
+			combination.clear();
+			combination.push_back(75);
+			combination.push_back(75);
+			combination.push_back(50);
+			combination.push_back(50);
+			combinations.push_back(combination);
+		}
+
 	}
 	bool containsCombination(const std::list<int> &input_combination)
 	{
-		return input_combination == combination;
+		for (auto it = combinations.begin(); it != combinations.end(); it++)
+		{
+			if (input_combination == *it) return true;
+		}
+		return false;
 	}
 };
 
@@ -39,8 +61,16 @@ TEST(TestCaseName, TestName) {
 }
 
 TEST(TestCaseWardrove, TestOnly50) {
-	Wall wall(50);
+	Wall wall({ 50 });
 	wall.combine();
 
+	EXPECT_TRUE(wall.containsCombination({ 50, 50, 50, 50, 50 }));
+}
+
+TEST(TestCaseWardrove, Test50_75) {
+	Wall wall({ 50,75 });
+	wall.combine();
+
+	EXPECT_TRUE(wall.containsCombination({ 75, 75, 50, 50 }));
 	EXPECT_TRUE(wall.containsCombination({ 50, 50, 50, 50, 50 }));
 }
